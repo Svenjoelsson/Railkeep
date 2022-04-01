6ad87a346c5d585f2eaaa3e62e3ad2a23cd1cca5
Svenjoelsson <76954999+Svenjoelsson@users.noreply.github.com>
2022-04-01 09:26:11 +0200

    Change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Dashboard1</h1>
+                <h1>Dashboard</h1>
             </div>
         </div>
     </div>
