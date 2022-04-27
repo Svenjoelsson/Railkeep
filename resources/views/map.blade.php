171b0752e1400f1b463a8790d111f01a538da669
Svenjoelsson <76954999+Svenjoelsson@users.noreply.github.com>
2022-04-27 15:21:33 +0200

    Update map.blade.php

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 
-<!--['lat' => 57.78326001570019, 'long' => 14.162159960732922, 'info' => 'Your Title'] -->
-
-
 @section('content')
-<x-maps-leaflet 
+<x-maps-leaflet
 zoomLevel="5" 
 :markers="$units" 
 :centerPoint="['lat' => 61.68051903629829, 'long' => 13.747171927808145]"
