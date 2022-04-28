aa28fdd7c5734f69905bfe3714c54590cb96a864
Svenjoelsson <76954999+Svenjoelsson@users.noreply.github.com>
2022-04-28 15:46:00 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -36,9 +36,9 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'gjerdeinvest.se'),
-            'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'host' => env('MAIL_HOST'),
+            'port' => env('MAIL_PORT'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
