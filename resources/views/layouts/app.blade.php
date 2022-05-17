4dd618b864282bbd365e73e94e4700e9b9a31be7
Svenjoelsson <76954999+Svenjoelsson@users.noreply.github.com>
2022-05-17 10:34:45 +0200

    Improvements to Create service customer select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -430,9 +430,12 @@ document.onreadystatechange = function() {
                 function(data2) {
                     $.each(data2.data, function(index, element2) {
                         if ($('.unitSelect').val() === element2.unit) {
-                            $('.customerSelect').val(element2.customer);
-                            $('.customerSelect').trigger('change.select2');
-                            $('.customerSelectDisable').prop("disabled", true);
+                            if (element2.customer) {
+                                $('.customerSelect').val(element2.customer);
+                                $('.customerSelect').trigger('change.select2');
+                                $('.customerSelectDisable').prop("disabled", true);
+                            }
+
 
                             // Load service types based on unit
                             $.get("{{ url('api/make_lists/') }}", function(data3) {
@@ -473,6 +476,41 @@ document.onreadystatechange = function() {
                     });
                 });
         });
+
+
+        $('.customerSelect').change(function() {
+            $.get("{{ url('api/contacts/') }}",
+                function(data2) {
+                    console.log(data2);
+                    $.each(data2.data, function(index, element2) {
+                        if ($('.customerSelect').val() === element2.customer) {
+                            if (element2.customer) {
+                                console.log(element2.customer);
+                            }
+
+
+
+
+                            // Load contacts connected to this customer.
+                            $.get("{{ url('api/contacts/') }}", function(data1) {
+                                $('.contactpersons').empty();
+                                $.each(data1.data, function(index1, element1) {
+                                    if (element1.customer == element2.customer) {
+                                    
+                                        $('.contactpersons').append($('<option>', {
+                                            value: element1.name,
+                                            text: element1.name
+                                        }));
+                                    }
+                                });
+                            });
+
+                        }
+                    });
+                });
+        });
+
+
 
         $('.inventoryUnit').change(function() {
             
