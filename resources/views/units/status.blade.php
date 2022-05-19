f5afb6ef50065ca92d4d0dd0dc604e43229905dc
Svenjoelsson <76954999+Svenjoelsson@users.noreply.github.com>
2022-05-19 10:07:46 +0200

    Update status.blade.php

--- resources/views/units/status.blade.php
+++ resources/views/units/status.blade.php
@@ -1,7 +1,7 @@
 <?php
     $critical = \App\Models\Services::where('unit', $unit)->where('service_status', 'In progress')->where('critical', '1')->whereNull('deleted_at')->first();
-    //$services = \App\Models\Services::where('unit', $unit)->whereNull('deleted_at')->whereNotNull('nextServiceDate')->orderBy('nextServiceDate', 'asc')->first();
-    //$services = \App\Models\Services::where('unit', $unit)->whereNull('deleted_at')->where('nextServiceDate', '>', now())->orderBy('nextServiceDate', 'asc')->first();
+    $repair = \App\Models\Services::where('unit', $unit)->where('service_status', 'In progress')->where('service_type', 'Repair')->where('service_type', 'Report')->whereNull('deleted_at')->first();
+
     $manual = \App\Models\Units::where('unit', $unit)->where('inService', '0')->whereNull('deleted_at')->first();
 
     $planned = \App\Models\Services::where('unit', $unit)->where('service_status', 'In progress')->where('service_date', '!=', '')->whereNull('deleted_at')->first();
@@ -27,6 +27,9 @@ else {
             echo '<a href="services/'.$critical->id.'/edit"><span style="font-size:16px;" data-toggle="tooltip" title="['.$critical->service_type.'] '.$critical->service_desc.'" class="badge bg"><i style="color:red;" class="fas fa-exclamation"></i></span></a>';
         }
     } 
+    else if ($repair) {
+        echo '<span style="font-size:16px;" class="badge bg-warning" data-toggle="tooltip" title="None critical repair/report in progress"><i style="color:white;" class="fas fa-exclamation"></i></span>';
+    }
     else if ($dateOverdue) {
         echo '<span style="font-size:16px;" class="badge bg-danger" data-toggle="tooltip" title="Service date overdue"><i class="fas fa-ban"></i></span>';
     }
